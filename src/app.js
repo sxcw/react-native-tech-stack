e971123214b3145da9369a6bf94ed06a0624c125
sxcw <yuan329419@gmail.com>
2017-08-14 17:11:54 -0500

    TechStack - Introduce LibraryList and add libraries reducer and dummy data

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
-  render() {
-    return (
+  return (
+    <Provider store={createStore(reducers)}>
       <View>
-        App!
+        <Header headerText='Tech Stack' />
+        <LibraryList />
       </View>
-    );
-  }
-};
+    </Provider>
+  )
+}
 
 export default App;

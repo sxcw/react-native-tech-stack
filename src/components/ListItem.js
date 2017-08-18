edee1dcb0eefe7737762c619ca003f6a8f7dae20
sxcw <yuan329419@gmail.com>
2017-08-17 21:21:37 -0500

    TectStack - Add render and styles to ListItem component

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Text } from 'react-native';
+import { CardSection } from './common';
 
 class ListItem extends Component {
   render() {
+    const { titleStyle } = styles;
 
+    return(
+      <CardSection>
+        <Text style={titleStyle}>
+          {this.props.library.title}
+        </Text>
+      </CardSection>
+    )
+  }
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
   }
 }
 

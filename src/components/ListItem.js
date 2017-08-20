b0bc9abcd084517fc3bb83a1cdfd8e811a5ee58b
sxcw <yuan329419@gmail.com>
2017-08-20 12:36:14 -0500

    TechStack - Add expand description functionality

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,9 +5,20 @@ import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <Text>{library.description}</Text>
+      )
+    }
+  }
+
   render() {
     const { titleStyle } = styles;
     const { id, title } = this.props.library;
+
     return(
       <TouchableWithoutFeedback
         onPress={() => this.props.selectLibrary(id)}>
@@ -17,6 +28,7 @@ class ListItem extends Component {
               {title}
             </Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     )
@@ -30,4 +42,9 @@ const styles = {
   }
 }
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded };
+}
+
+export default connect(mapStateToProps, actions)(ListItem);
